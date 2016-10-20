94d4768dab45ce0b2ec8d3a18d1968afa5dc98c3
jianqing <qing.jian@ptthink.com>
2016-10-20 22:01:10 +0800

    feat:插件结构搭建

--- src/main.js
+++ src/main.js
@@ -1,1 +1,46 @@
 //jianqing
+
+;(function($){
+
+    var methods = {
+
+        //function
+        destroy: function(){},
+        disable: function(){},
+        enable: function(){},
+        getOption: function(){},
+        add: function(){},
+        delete: function(){},
+        edit: function(){},
+
+        //event
+        create: function(){},
+        drag: function(){},
+        start: function(){},
+        stop: function(){},
+        selected: function(){}
+    };
+
+    $.fn.Draglist = function(option,methods){
+        this.VERSION = '1.0.0';
+
+        //option
+        this.option ={
+            type: option.type || '',
+            disable: option.disable  || 'false',
+            handle: option.handle,
+            axis: option.axis || 'horizontal',
+            cursor: option.cursor || 'pointer',
+            delay: option.delay || 300,
+            opacity: option.opacity || 0.5,
+            scroll: option.scroll || 'false',
+            stack: option.stack || 2
+        };
+
+        var options = $.extend({
+            zIndex: 2
+        },option);
+
+        return this;
+    }
+})(jQuery);
